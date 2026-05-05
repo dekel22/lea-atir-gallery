23543555e7d2912bd918c0b4cab6694d86284057
dekel22 <dekell8@gmail.com>
2026-05-05 17:05:28 +0300

    כג

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,19 @@ import Gallery from './pages/Gallery';
 function App() {
   return (
     <Router>
-      <div dir="rtl" className="min-h-screen flex flex-col">
+      <div dir="rtl" className="min-h-screen flex flex-col relative">
+        {/* Semi-transparent background image */}
+        <div 
+          className="fixed inset-0 pointer-events-none z-[-1]" 
+          style={{
+            backgroundImage: "url('/semi-transparent.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            opacity: 0.15
+          }}
+        />
         <Navbar />
-        <main className="flex-1">
+        <main className="flex-1 relative z-10">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/gallery/:id" element={<Gallery />} />
