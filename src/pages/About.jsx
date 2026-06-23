3d57fd5d15ffaf3f6b950dac35b92aac41aeb2b0
spabeitoren-spa <spabeitoren@gmail.com>
2026-06-23 15:39:04 +0300

    Restored B&W self-portrait and original caption on About page only

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import portraitImg from '../assets/about-portrait.jpg';
+import portraitImg from '../assets/about-portrait-bw.jpg';
 
 const About = () => {
   const { t, i18n } = useTranslation();
